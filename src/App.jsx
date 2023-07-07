400b7fb87e6a5edb82f74367d955f72d6b01c0a6
Thimet <92925044+Thimet@users.noreply.github.com>
2023-07-07 22:40:02 +0530

    did some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,2 @@
-import Login from "./pages/Login"
-import Dashboard from "./pages/dashboard";
-import Forgotpassword from "./pages/Forgotpassword";
-import Setnewpassword from "./pages/Setnewpassword";
-import Verifyemail from "./pages/Verifyemail";
-import Signup from "./pages/Signup";
-import Slideshow from "./components/Slideshow";
-import Navbar from "./components/Navbar";
-import Slideshow2 from "./components/Slideshow2";
-import Landingbottomdetails from "./components/Landingbottomdetails";
-const App=() =>{
-
-  return (  
-    <div>
-        {/* <Forgotpassword/> */}
-        {/* <Verifyemail/> */}
-        {/* <Dashboard /> */}
-        {/* <Login /> */}
-        {/* <Setnewpassword/> */}
-        {/* <Signup/> */}
-        <Navbar/>
-        <Slideshow/>
-        <Slideshow2/>
-        <Landingbottomdetails/>
-    </div>
-  )
-}
 
 export default App;
