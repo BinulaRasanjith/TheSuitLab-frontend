45a3187f398dbcf0ce7bfa71e15761044be963aa
Thimet <92925044+Thimet@users.noreply.github.com>
2023-07-07 22:47:11 +0530

    create landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Slideshow from './components/Slideshow'
+import Landingbottomdetails from './components/Landingbottomdetails'
+import Slideshow2 from './components/Slideshow2'
 
-export default App;
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Slideshow />
+      <Slideshow2 />
+      <Landingbottomdetails />
+      
+    </div>
+  )
+}
+
+export default App
