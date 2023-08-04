306dc709c5536bcd0155d6e3ee8ff96adfeaed1b
Thimet <92925044+Thimet@users.noreply.github.com>
2023-08-04 08:10:56 +0530

    Changed 3D model

--- src/components/UpperModel.jsx
+++ src/components/UpperModel.jsx
@@ -18,26 +18,8 @@ const UpperModel = () => {
         scale: 2.3,
     })
     return (
-<<<<<<< HEAD
-        
-            <div className="w-1/3 h-screen flex items-center relative">
-                <ControlPanel control={control} setControl={setControl} />
-                <CamControl camCont={camCont} setCamCont={setCamCont} />
-                <Canvas camera={{ position: [0, 0, 10], fov: 30 }}>
-                    <Suspense fallback={false}>
-                        {/* <Suit1 controller={controller} /> */}
-                        <NewCostume camCont={camCont} control={control} />
-                    </Suspense>
-                    <OrbitControls enableZoom={true} makeDefault maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-                    <Environment preset="city" />
-                    {/* <ContactShadows rotation-x={Math.PI / 2} position={[0, -4.5, 0]} opacity={0.25} width={10} height={10} blur={1} far={4.5} /> */}
-                </Canvas>
 
-                <BackNext/>
-                
-            </div>
-=======
-        <div className="w-1/3 h-screen flex items-center py-10 relative mx-20">
+        <div className="w-1/3 h-screen flex items-center relative">
             <ControlPanel control={control} setControl={setControl} />
             <CamControl camCont={camCont} setCamCont={setCamCont} />
             <Canvas camera={{ position: [0, 0, 10], fov: 30 }}>
@@ -49,8 +31,10 @@ const UpperModel = () => {
                 <Environment preset="city" />
                 {/* <ContactShadows rotation-x={Math.PI / 2} position={[0, -4.5, 0]} opacity={0.25} width={10} height={10} blur={1} far={4.5} /> */}
             </Canvas>
+
+            <BackNext />
+
         </div>
->>>>>>> e5c91c30f94672d2598b663015582ab57d19e2ef
     )
 }
 
