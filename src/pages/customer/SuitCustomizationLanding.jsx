46cc35681af559ee718751cd4ee2d79f80c726d2
Thimet <92925044+Thimet@users.noreply.github.com>
2023-08-06 23:29:07 +0530

    Made few Changes

--- src/pages/customer/SuitCustomizationLanding.jsx
+++ src/pages/customer/SuitCustomizationLanding.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 import Handsome from '../../assets/images/portrait-handsome-fashion-stylish-hipster-businessman-model-dressed-elegant-blue-suit-posing-gray 1.png'
 
+
 const SuitCustomizationLanding = () => {
+
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+
     return (
         <div>
             <img alt="" className="h-screen w-screen relative" src={Handsome} />
@@ -12,7 +24,29 @@ const SuitCustomizationLanding = () => {
                     Our suits are not only high-quality but they are also hand
                     tailored to your measurements. Choose from more than 150
                     fabrics and various details and design your own outfit.</div>
-                <button className='px-10 py-2.5 rounded-3xl bg-gray-400 md:mx-10 font-bold'>Design Your Suit</button>
+
+                <button className="md:mx-10 border-2 border-black font-bold text-black bg-transparent rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center w-44" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" id="dropdownHoverButton" onClick={toggleDropdown} type="button">Dropdown hover <svg aria-hidden="true" className="w-2.5 h-2.5 ml-2.5" fill="none" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m1 1 4 4 4-4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                </svg></button>
+
+                <div className={`md:mx-10 ${isOpen ? 'opacity-100' : 'opacity-0'
+                    } z-10 border-2 mt-5 font-bold border-black bg-transparent rounded-lg shadow w-44 transition-opacity duration-300`} id="dropdownHover">
+                    <ul aria-labelledby="dropdownHoverButton" className="py-2 text-sm text-black">
+                        <li>
+                            <a className="block px-4 py-2 hover:bg-gray-100" href="#">Dashboard</a>
+                        </li>
+                        <li>
+                            <a className="block px-4 py-2 hover:bg-gray-100" href="#">Settings</a>
+                        </li>
+                        <li>
+                            <a className="block px-4 py-2 hover:bg-gray-100" href="#">Earnings</a>
+                        </li>
+                        <li>
+                            <a className="block px-4 py-2 hover:bg-gray-100" href="#">Sign out</a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     )
