cb032710194278d8f198c292bc654f2a2c6fa10b
Binula Rasanjith <binula28@gmail.com>
2023-08-09 14:02:59 +0530

    card container grdi made resppnsive

--- src/components/MaterialCard/CardContainer.jsx
+++ src/components/MaterialCard/CardContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const CardContainer = ({ children }) => {
     return (
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-8 items-center justify-center'>
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(180px,_240px))] gap-6 px-6 justify-center'>
             {children}
         </div>
     )
