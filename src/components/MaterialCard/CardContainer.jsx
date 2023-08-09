6e592f407841bc19f597114081e6de6c2d6ecf6f
Binula Rasanjith <binula28@gmail.com>
2023-08-09 09:31:15 +0530

    card container changed to a grid

--- src/components/MaterialCard/CardContainer.jsx
+++ src/components/MaterialCard/CardContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const CardContainer = ({ children }) => {
     return (
-        <div className='flex flex-wrap gap-8 px-8 items-center justify-center'>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-8 items-center justify-center'>
             {children}
         </div>
     )
