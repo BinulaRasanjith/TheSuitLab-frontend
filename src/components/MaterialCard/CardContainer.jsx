308562f93225748a0afa8da32a3c120732e0cce2
Binula Rasanjith <binula28@gmail.com>
2023-08-11 10:08:32 +0530

    minimal changes

--- src/components/MaterialCard/CardContainer.jsx
+++ src/components/MaterialCard/CardContainer.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const CardContainer = ({ children }) => {
     return (
-        <div className='grid grid-cols-[repeat(auto-fit,_minmax(180px,_240px))] gap-6 px-6 justify-center'>
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-6 px-6 justify-center'>
             {children}
         </div>
     )
--- src/routes/tailorRoutes.jsx
+++ src/routes/tailorRoutes.jsx
@@ -1,76 +1,6 @@
 
-// import SingleOutletLayout from "../layouts/SingleOutletLayout"
-// import TailorMainLayout from "../layouts/tailorLayouts/TailorMainLayout"
-// import Fabrics from "../pages/tailor/Fabrics"
-// import Buttons from "../pages/tailor/Buttons"
-// import Orders from "../pages/Tailor/Orders"
-
-// const tailorRoutes = {
-//     path: '/tailor',
-//     element: <TailorMainLayout />,
-//     children: [
-//         {
-//             path: 'dashboard',
-//             element: <h1>Dashboard</h1>
-//         },
-//         {
-//             path: 'orders',
-//             element: <SingleOutletLayout />,
-//             children: [
-//                 {
-//                     path: 'all',
-//                     element: <h1>All Orders</h1>
-//                 },
-//                 {
-//                     path: 'collected',
-//                     element: <h1>Collected Orders</h1>
-//                 },
-//                 {
-//                     path: 'completed',
-//                     element: <h1>Completed Orders</h1>
-//                 },
-//                 {
-//                     path: 'to-be-collected',
-//                     element: <h1>To Be Canceled Orders</h1>
-//                 },
-//             ]
-//         },
-//         {
-//             path: 'materials',
-//             element: <SingleOutletLayout />,
-//             children: [
-//                 {
-//                     path: 'Fabric',
-//                     element: <Fabrics />
-//                 },
-//                 {
-//                     path: 'Strings',
-//                     element: <h1>String</h1>
-//                 },
-//                 {
-//                     path: 'Buttons',
-//                     element: <Buttons />
-//                 },
-//             ]
-//         },
-//         {
-//             path: 'reviews',
-//             element: <h1>Reviews</h1>
-//         },
-//         {
-//             path: 'profile',
-//             element: <h1>Profile</h1>
-//         },
-//     ]
-// =======
-
-
-//layouts
-import TailorMainLayout from "../layouts/tailorLayouts/TailorMainLayouts"
-import MaterialsLayout from "../layouts/tailorLayouts/MaterialsLayout"
-
-
-//pages
+import SingleOutletLayout from "../layouts/SingleOutletLayout"
+import TailorMainLayout from "../layouts/tailorLayouts/TailorMainLayout"
 import TailorDashboard from "../pages/Tailor/Dashboard"
 import Orders from "../pages/Tailor/Orders"
 import Profile from "../pages/Tailor/Profile"
@@ -79,27 +9,63 @@ import Buttons from "../pages/tailor/Buttons"
 import Fabrics from "../pages/tailor/Fabrics"
 import TEST from "../pages/tailor/test"
 
-
 const tailorRoutes = {
-    path: "/tailor",
+    path: '/tailor',
     element: <TailorMainLayout />,
     children: [
-        { path: "dashboard", element: <TailorDashboard /> },
-        { path: "orders", element: <Orders /> },
-        { path: "profile", element: <Profile /> },
-        { 
-            path: "materials", 
-            element: <MaterialsLayout />,
+        {
+            path: 'dashboard',
+            element: <TailorDashboard />
+        },
+        {
+            path: 'orders',
+            element: <SingleOutletLayout />,
             children: [
-                { path: "fabric", element: <Fabrics/> },
-                { path: "string", element: <div>String</div> },
-                { path: "buttons", element: <Buttons/> },
-                { path: "colors", element: <div>Color contrast</div> },
-            ]    
-    },  
-        { path: "reviews", element: <Reviews /> },
-        { path: "test", element: <TEST /> }, 
-    ],
+                {
+                    path: 'all',
+                    element: <Orders />
+                },
+                {
+                    path: 'collected',
+                    element: <h1>Collected Orders</h1>
+                },
+                {
+                    path: 'completed',
+                    element: <h1>Completed Orders</h1>
+                },
+                {
+                    path: 'to-be-collected',
+                    element: <h1>To Be Canceled Orders</h1>
+                },
+            ]
+        },
+        {
+            path: 'materials',
+            element: <SingleOutletLayout />,
+            children: [
+                {
+                    path: 'Fabric',
+                    element: <Fabrics />
+                },
+                {
+                    path: 'Strings',
+                    element: <h1>String</h1>
+                },
+                {
+                    path: 'Buttons',
+                    element: <Buttons />
+                },
+            ]
+        },
+        {
+            path: 'reviews',
+            element: <Reviews />
+        },
+        {
+            path: 'profile',
+            element: <Profile />
+        },
+    ]
 }
 
 export default tailorRoutes
