b5c2d7220e4778760d346dc2121e61ceff58cb7a
Binula Rasanjith <binula28@gmail.com>
2023-08-12 13:02:12 +0530

    add employee half developed

--- src/pages/Admin/AddEmployee.jsx
+++ src/pages/Admin/AddEmployee.jsx
@@ -1,10 +1,30 @@
+import { Button } from "@chakra-ui/button"
 
+import { addUser } from "../../api/userAPI"
 
 const AddEmployee = () => {
+    const handleAddUserClick = async () => {
+        const user = {
+            mobileNo: "0123456893434",
+            firstName: "First",
+            lastName: "Last",
+            role: "TAILOR",
+            password: "password"
+        }
+
+        addUser(user)
+            .then((res) => {
+                console.log(res)
+            })
+    }
+
     return (
         <>
             <h1>Add Employee</h1>
 
+            <Button onClick={handleAddUserClick}>
+                add
+            </Button>
         </>
     )
 }
