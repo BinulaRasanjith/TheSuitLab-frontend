77b6c0ad6bf05d68ba7dcaa18986575e982039d6
Binula Rasanjith <binula28@gmail.com>
2023-08-13 18:24:02 +0530

    constants updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 // user roles
 export const ADMIN = 'admin';
 export const CUSTOMER = 'customer';
-export const PRODUCT_MANAGER = 'product-manager';
-export const OPERATION_ASSISTANT = 'operation-assistant';
+export const PRODUCT_MANAGER = 'manager';
+export const OPERATION_ASSISTANT = 'assistant';
 export const TAILOR = 'tailor';
 
 // user status
