83b8057dce6f8afac1b52e01441bf9de51182b36
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-08-13 17:34:21 +0530

    UI fix

--- src/components/TopNavBar/Staffnavbar.jsx
+++ src/components/TopNavBar/Staffnavbar.jsx
@@ -32,6 +32,8 @@ const Staffnavbar = () => {
     }
 
     const [open, setOpen] = useState(false);
+    const [isUserDropdownOpen, setUserDropdownOpen] = useState(false);
+
 
     return (
         <>
@@ -51,27 +53,27 @@ const Staffnavbar = () => {
                         <div className='text-white text-end'>Bhanuka Rajakaruna</div>
                         <div className='text-gray-400 text-xs text-end'>Operation Assistant</div>
                     </div>
-                    <img id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="w-10 h-10 rounded-full cursor-pointer" src={AVATAR} alt="User dropdown" />
+                    <img id="avatarButton" type="button" onClick={() => setUserDropdownOpen(!isUserDropdownOpen)} data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" className="w-10 h-10 rounded-full cursor-pointer" src={AVATAR} alt="User dropdown" />
 
                     {/*  Dropdown menu  */}
-                    <div id="userDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                        <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                            <div>Bonnie Green</div>
-                            <div className="font-medium truncate">name@flowbite.com</div>
+                    <div id="userDropdown" className="z-10 ${isUserDropdownOpen ? 'block' : 'hidden'} hidden bg-white divide-y divide-gray-300 rounded-lg shadow w-44 ">
+                        <div className="px-4 py-3 text-sm text-gray-900">
+                            <div>Bhanuka Rajakaruna</div>
+                            <div className="font-medium truncate">bhanukayar@gmail.com</div>
                         </div>
                         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
                             <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Dashboard</a>
                             </li>
                             <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Settings</a>
                             </li>
                             <li>
-                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100 ">Earnings</a>
                             </li>
                         </ul>
                         <div className="py-1">
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 ">Sign out</a>
                         </div>
                     </div>
                     {/* <Button
--- src/pages/Assistant/Profile.jsx
+++ src/pages/Assistant/Profile.jsx
@@ -1,16 +1,38 @@
 /* eslint-disable perfectionist/sort-jsx-props */
+import AVATAR from '../../assets/images/avatar.png'
+
+
 const Profile = () => {
     return (
         <div>
-            <div className=" flex flex-col justify-between w-full p-6 gap-6">
+            <div className=" flex flex-col justify-between w-full p-10 gap-6">
 
 
-                <div className=" flex justify-center">
-                    <div></div>
+                <div className=" flex justify-start">
+                    <div>
+                        {/* <div className="text-center">
+                            <img
+                                src={AVATAR}
+                                className="mx-auto mb-4 w-28 border border-zinc-500 rounded-lg"
+                                alt="Avatar" />
+                            <h5 className="mb-2 text-xl font-medium leading-tight">Bhanuka Rajakaruna</h5>
+                            <p className="text-neutral-500 dark:text-neutral-400">Operation Assistant</p>
+                        </div> */}
+                        <div className=" flex justify-center gap-12 mx-5">
+                            <div className=" border-neutral-200 rounded-lg"><img
+                                src={AVATAR}
+                                className="mx-auto mb-4 w-36 border border-zinc-500 rounded-lg"
+                                alt="Avatar" /></div>
+                            <div className=' flex flex-col justify-center'>
+                                <div><h5 className="mb-2 text-xl font-medium leading-tight">Bhanuka Rajakaruna</h5></div>
+                                <div><p className="text-neutral-500 dark:text-neutral-400">Operation Assistant</p></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className=" flex gap-6">
-                    <div className=" border border-solid rounded flex flex-col gap-5 bg-gray-50 px-7 py-9 w-1/2">
+                <div className=" flex gap-12">
+                    <div className=" border border-solid rounded flex flex-col gap-5 bg-gray-50 px-7 py-9 w-full">
                         <div>
                             <label htmlFor="currency" className=" ">First Name</label>
                             <input
@@ -46,39 +68,39 @@ const Profile = () => {
                         </div>
                     </div>
 
-                    <div className=" border border-solid rounded flex flex-col gap-5 bg-gray-50 px-7 py-9 w-1/2">
+                    <div className=" border border-solid rounded flex flex-col gap-5 bg-gray-50 px-7 py-9 w-full">
                         <div>
-                            <label htmlFor="currency" className=" ">First Name</label>
+                            <label htmlFor="currency" className=" ">Current Password</label>
                             <input
-                                type="text"
+                                type="password"
                                 name="price"
                                 id="price"
-                                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder="Pasan"
+                                className="block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Current Password"
                             />
                         </div>
                         <div>
-                            <label htmlFor="currency" className=" ">Last Name</label>
+                            <label htmlFor="currency" className=" ">New Password</label>
                             <input
-                                type="text"
+                                type="password"
                                 name="price"
                                 id="price"
-                                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder="Gamage"
+                                className="block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="New Password"
                             />
                         </div>
                         <div>
-                            <label htmlFor="currency" className=" ">Mobile</label>
+                            <label htmlFor="currency" className=" ">Confirm Password</label>
                             <input
-                                type="text"
+                                type="password"
                                 name="price"
                                 id="price"
-                                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                placeholder="0764927560"
+                                className="block w-full rounded-md border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Confirm New Password"
                             />
                         </div>
                         <div>
-                            <button className=" w-full rounded-md border-0 py-2 text-gray-100 bg-black">Update Now</button>
+                            <button className=" w-full rounded-md border-0 py-2 text-gray-100 bg-black" >Change Password</button>
                         </div>
                     </div>
                 </div>
