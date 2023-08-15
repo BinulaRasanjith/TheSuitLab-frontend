8953e07168b8298babc9eb909df972319946b23d
Affadha <affadha.1967@gmail.com>
2023-08-15 10:54:21 +0530

    tailorOrders12

--- src/pages/tailor/Orders/CollectedOrders.jsx
+++ src/pages/tailor/Orders/CollectedOrders.jsx
@@ -6,8 +6,8 @@ const AllOrders = () => {
             <div className=" flex flex-col justify-between mx-10 my-8 p-5 border border-solid border-zinc-950 border-opacity-20 rounded-lg">
                 <div className=" flex justify-between align-middle pb-5">
                     <div className='flex flex-col'>
-                        <div className=' text-2xl font-semibold'>Materials</div>
-                        <div className=' text-sm font-regular text-blue-400'>Materials should be updated</div>
+                        <div className=' text-2xl font-semibold'>All orders</div>
+                        <div className=' text-sm font-regular text-blue-400'>Recent orders</div>
                     </div>
                     {/* <div className=" flex gap-4 align-middle">
                             <div>
@@ -20,11 +20,11 @@ const AllOrders = () => {
                 </div>
                 <div className=" flex flex-col">
                     <div className=' flex justify-between text-sm font-medium text-gray-400'>
-                        <div className=" w-40 text-">Material Name</div>
-                        <div className=" w-32 text-">Material Code</div>
-                        <div className=" w-32 text-">Supplier Id</div>
-                        <div className=" w-32 text-">Supplier mobile</div>
-                        <div className=" w-32 text-">Last Order</div>
+                        <div className=" w-40 text-">Order Id</div>
+                        <div className=" w-32 text-">Item Count</div>
+                        <div className=" w-32 text-">Phone Number</div>
+                        <div className=" w-32 text-">Customer</div>
+                        <div className=" w-32 text-">Ordered Date</div>
                         <div className=" w-40 px-10">Status</div>
                     </div>
                     <div className=" w-full border h-0 mt-3 mb-6 border-gray-200"></div>
