64bb19fb058a00e807b7359bb8e26218674d56d8
Binula Rasanjith <binula28@gmail.com>
2023-08-17 04:49:34 +0530

    materialAPIs added

--- src/config/config.js
+++ src/config/config.js
@@ -3,3 +3,5 @@ export const SERVER_URL = 'http://localhost:3333'; // TODO: get this from .env
 export const API_URL = `${SERVER_URL}/api`; // TODO: use this
 
 export const PROFILE_PICTURE_URL = `${SERVER_URL}/uploads/profile-pics`;
+
+export const MATERIAL_IMAGES_URL = `${SERVER_URL}/uploads/materials-images`;
