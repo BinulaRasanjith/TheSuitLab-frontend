0acabdf878c2c3d855d819378e9ef96b594edab7
Binula Rasanjith <binula28@gmail.com>
2023-08-17 03:22:27 +0530

    employees details rtieved and employee container css changed

--- src/components/EmployeeCard/CardContainer.jsx
+++ src/components/EmployeeCard/CardContainer.jsx
@@ -1,15 +1,15 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 const CardContainer = ({ children }) => {
-    return (
-        <div className='grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-6 px-6 justify-center'>
-            {children}
-        </div>
-    )
-}
+	return (
+		<div className="grid grid-cols-[repeat(auto-fit,_minmax(150px,_200px))] gap-6 px-6">
+			{children}
+		</div>
+	);
+};
 
 CardContainer.propTypes = {
-    children: PropTypes.node.isRequired
-}
+	children: PropTypes.node.isRequired,
+};
 
-export default CardContainer
+export default CardContainer;
--- src/pages/Admin/Employees.jsx
+++ src/pages/Admin/Employees.jsx
@@ -1,145 +1,175 @@
-import { Button } from "@chakra-ui/react"
-import { AiFillPlusCircle } from 'react-icons/ai'
-import { useNavigate } from "react-router"
+import { Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useNavigate } from "react-router";
 
-import Costume1 from '../../assets/images/costume1.jpeg'
-import Costume2 from '../../assets/images/costume2.jpeg'
-import Costume3 from '../../assets/images/costume3.jpeg'
-import Costume4 from '../../assets/images/costume4.jpeg'
-import Costume5 from '../../assets/images/costume5.jpeg'
-import CardContainer from "../../components/EmployeeCard/CardContainer"
-import EmployeeCard from "../../components/EmployeeCard/EmployeeCard"
+import { getUsers } from "../../api/userAPI";
+import Costume1 from "../../assets/images/costume1.jpeg";
+import Costume2 from "../../assets/images/costume2.jpeg";
+import Costume3 from "../../assets/images/costume3.jpeg";
+import Costume4 from "../../assets/images/costume4.jpeg";
+import Costume5 from "../../assets/images/costume5.jpeg";
+import CardContainer from "../../components/EmployeeCard/CardContainer";
+import EmployeeCard from "../../components/EmployeeCard/EmployeeCard";
+import { PROFILE_PICTURE_URL } from "../../config/config";
+import { OPERATION_ASSISTANT, PRODUCT_MANAGER, TAILOR } from "../../constants";
+import displayRoleName from "../../utils/displayRoleName";
 
 const employees = [
-    {
-        name: "Chamindu Sandaruwan",
-        role: "Product Manager",
-        image: Costume1
-    },
-    {
-        name: "Oshada Fernando",
-        role: "Product Manager",
-        image: Costume2
-    },
-    {
-        name: "Ravindra Gamage",
-        role: "Product Manager",
-        image: Costume3
-    },
-    {
-        name: "Kushan Liyanage",
-        role: "Product Manager",
-        image: Costume4
-    }
-
+	{
+		name: "Chamindu Sandaruwan",
+		role: "Product Manager",
+		image: Costume1,
+	},
+	{
+		name: "Oshada Fernando",
+		role: "Product Manager",
+		image: Costume2,
+	},
+	{
+		name: "Ravindra Gamage",
+		role: "Product Manager",
+		image: Costume3,
+	},
+	{
+		name: "Kushan Liyanage",
+		role: "Product Manager",
+		image: Costume4,
+	},
 ];
-
 
 const TailorData = [
-    {
-        name: "Shayma Dissanayake",
-        role: "Head Tailor",
-        image: Costume5
-    },
-    {
-        name: "Dilahara Ranaweera",
-        role: "Tailoring Supervisor",
-        image: Costume5
-    },
-    {
-        name: "Thushara Udayanga",
-        role: "Head Tailor",
-        image: Costume1
-    },
-    {
-        name: "Jayanni Hemanthi",
-        role: "Tailoring Supervisor",
-        image: ""
-    }
-
+	{
+		name: "Shayma Dissanayake",
+		role: "Head Tailor",
+		image: Costume5,
+	},
+	{
+		name: "Dilahara Ranaweera",
+		role: "Tailoring Supervisor",
+		image: Costume5,
+	},
+	{
+		name: "Thushara Udayanga",
+		role: "Head Tailor",
+		image: Costume1,
+	},
+	{
+		name: "Jayanni Hemanthi",
+		role: "Tailoring Supervisor",
+		image: "",
+	},
 ];
 
-
 const Employees = () => {
-    const navigate = useNavigate()
-    const handleAddUserClick = () => {
-        navigate('/admin/add-employee')
-    }
+	const navigate = useNavigate();
 
-    return (
+	const [managers, setManagers] = useState([]);
+	const [tailors, setTailors] = useState([]);
+	const [assistants, setAssistants] = useState([]);
 
-        <>
-            <div className="flex items-center justify-between">
-                <h1 className="text-xl font-semibold p-7">Product Manager</h1>
+	const handleAddUserClick = () => {
+		navigate("/admin/add-employee");
+	};
 
-                <Button
-                    leftIcon={<AiFillPlusCircle />}
-                    m={3}
-                    onClick={(handleAddUserClick)}
-                    rounded={'full'}
-                    color={'white'}
-                    bgColor={"black"}
-                    _hover={
-                        {
-                            bg: 'blue.500',
-                        }
-                    }
-                >
-                    Add User
-                </Button>
-            </div>
+	useEffect(() => {
+		const getEmployees = async () => {
+			const response = await getUsers({
+				roles: [PRODUCT_MANAGER, TAILOR, OPERATION_ASSISTANT],
+			});
 
-            {/* cards*/}
+			const employees = response.data.users;
 
-            <CardContainer>
-                {employees.map((employee, index) => {
-                    return (
-                        <EmployeeCard
-                            image={employee.image}
-                            key={index}
-                            EmpName={employee.name}
-                            role={employee.role}
-                        />
-                    )
-                })}
-            </CardContainer>
+			const managers = employees.filter(
+				(employee) => employee.role === PRODUCT_MANAGER
+			);
+			const tailors = employees.filter((employee) => employee.role === TAILOR);
+			const assistants = employees.filter(
+				(employee) => employee.role === OPERATION_ASSISTANT
+			);
 
-            <div className="flex flex-row mt-5 justify-start">
-                <h1 className="text-xl font-semibold p-7">Tailors & Supervisors</h1>
-            </div>
+			setManagers(managers);
+			setTailors(tailors);
+			setAssistants(assistants);
 
-            <CardContainer>
-                {TailorData.map((employee, index) => {
-                    return (
-                        <EmployeeCard
-                            image={employee.image}
-                            key={index}
-                            EmpName={employee.name}
-                            role={employee.role}
-                        />
-                    )
-                })}
-            </CardContainer>
+			console.log(managers);
+			console.log(tailors);
+			console.log(assistants);
+		};
 
-            <div className="flex flex-row mt-5 justify-start">
-                <h1 className="text-xl font-semibold p-7">Operation Assistant</h1>
-            </div>
+		getEmployees();
+	}, []);
 
-            <CardContainer>
-                {TailorData.map((employee, index) => {
-                    return (
-                        <EmployeeCard
-                            image={employee.image}
-                            key={index}
-                            EmpName={employee.name}
-                            role={employee.role}
-                        />
-                    )
-                })}
-            </CardContainer>
-        </>
-    )
-}
+	return (
+		<>
+			<div className="flex items-center justify-between">
+				<h1 className="text-xl font-semibold p-7">Product Manager</h1>
 
+				<Button
+					leftIcon={<AiFillPlusCircle />}
+					m={3}
+					onClick={handleAddUserClick}
+					rounded={"full"}
+					color={"white"}
+					bgColor={"black"}
+					_hover={{
+						bg: "blue.500",
+					}}
+				>
+					Add User
+				</Button>
+			</div>
 
-export default Employees
+			{/* cards*/}
+
+			<CardContainer>
+				{managers.map((employee, index) => {
+					return (
+						<EmployeeCard
+							key={index}
+							image={`${PROFILE_PICTURE_URL}/${employee.image}`}
+							EmpName={employee.firstName + " " + employee.lastName}
+							role={displayRoleName(employee.role)}
+						/>
+					);
+				})}
+			</CardContainer>
+
+			<div className="flex flex-row mt-5 justify-start">
+				<h1 className="text-xl font-semibold p-7">Tailors & Supervisors</h1>
+			</div>
+
+			<CardContainer>
+				{tailors.map((employee, index) => {
+					return (
+						<EmployeeCard
+							key={index}
+							image={`${PROFILE_PICTURE_URL}/${employee.image}`}
+							EmpName={employee.firstName + " " + employee.lastName}
+							role={displayRoleName(employee.role)}
+						/>
+					);
+				})}
+			</CardContainer>
+
+			<div className="flex flex-row mt-5 justify-start">
+				<h1 className="text-xl font-semibold p-7">Operation Assistant</h1>
+			</div>
+
+			<CardContainer>
+				{assistants.map((employee, index) => {
+					return (
+						<EmployeeCard
+							key={index}
+							image={`${PROFILE_PICTURE_URL}/${employee.image}`}
+							EmpName={employee.firstName + " " + employee.lastName}
+							role={displayRoleName(employee.role)}
+						/>
+					);
+				})}
+			</CardContainer>
+		</>
+	);
+};
+
+export default Employees;
