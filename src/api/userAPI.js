bc0bde4595907dba944cc18dd690c8d5ac7f4e98
Shamalka_M <2020cs111@stu.ucsc.cmb.ac.lk>
2023-08-16 18:53:40 +0530

    upload profile pic

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,7 +1,12 @@
 import api from "./api";
 
 export const addUser = (data) => {
-    return api.post('/user/add-user', data);
+    //console.log(data);
+    return api.post('/user/add-user', data, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
 }
 
 export const getUsers = (data) => {
