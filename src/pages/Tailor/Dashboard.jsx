082ef0a394240286e9dfc6492b47f12f9f234145
Affadha <affadha.1967@gmail.com>
2023-08-16 16:15:40 +0530

    tailordashboard

--- src/pages/Tailor/Dashboard.jsx
+++ src/pages/Tailor/Dashboard.jsx
@@ -1,4 +1,5 @@
-import staffCard from "../../components/staffCard";
+import bar from "../../assets/images/bar.png"
+import chart from "../../assets/images/chart.png"
 
 const Dashboard = () => {
 
@@ -18,8 +19,8 @@ const Dashboard = () => {
                   <div className=' text-xs font-medium text-gray-400'>Since last week</div>
                 </div>
                 <div className="text-black">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                  <svg className="w-7 h-7" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
 
                 </div>
@@ -34,8 +35,8 @@ const Dashboard = () => {
 
                 </div>
                 <div className="text-black">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                  <svg className="w-7 h-7" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
 
                 </div>
@@ -50,8 +51,8 @@ const Dashboard = () => {
 
                 </div>
                 <div className="text-black">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M7.848 8.25l1.536.887M7.848 8.25a3 3 0 11-5.196-3 3 3 0 015.196 3zm1.536.887a2.165 2.165 0 011.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 11-5.196 3 3 3 0 015.196-3zm1.536-.887a2.165 2.165 0 001.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863l2.077-1.199m0-3.328a4.323 4.323 0 012.068-1.379l5.325-1.628a4.5 4.5 0 012.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.331 4.331 0 0010.607 12m3.736 0l7.794 4.5-.802.215a4.5 4.5 0 01-2.48-.043l-5.326-1.629a4.324 4.324 0 01-2.068-1.379M14.343 12l-2.882 1.664" />
+                  <svg className="w-7 h-7" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.848 8.25l1.536.887M7.848 8.25a3 3 0 11-5.196-3 3 3 0 015.196 3zm1.536.887a2.165 2.165 0 011.083 1.839c.005.351.054.695.14 1.024M9.384 9.137l2.077 1.199M7.848 15.75l1.536-.887m-1.536.887a3 3 0 11-5.196 3 3 3 0 015.196-3zm1.536-.887a2.165 2.165 0 001.083-1.838c.005-.352.054-.695.14-1.025m-1.223 2.863l2.077-1.199m0-3.328a4.323 4.323 0 012.068-1.379l5.325-1.628a4.5 4.5 0 012.48-.044l.803.215-7.794 4.5m-2.882-1.664A4.331 4.331 0 0010.607 12m3.736 0l7.794 4.5-.802.215a4.5 4.5 0 01-2.48-.043l-5.326-1.629a4.324 4.324 0 01-2.068-1.379M14.343 12l-2.882 1.664" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
 
                 </div>
@@ -66,8 +67,8 @@ const Dashboard = () => {
 
                 </div>
                 <div className=" text-black">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                  <svg className="w-7 h-7" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" strokeLinecap="round" strokeLinejoin="round" />
                   </svg>
 
                 </div>
@@ -79,13 +80,17 @@ const Dashboard = () => {
             {/*grid start here*/}
             <div className="mt-5   grid lg:grid-cols-3 md:grid-cols-2 p-4 gap-2">
               <div className="bg-white p-8 flex-col rounded shadow-lg">
-                <b className='flex flex-row text-gray-500'>PERFOMANCE</b></div>
-              <canvas className='p-5' id='chartLine'></canvas>
+                <b className='flex flex-row text-gray-500'>PERFOMANCE</b>
+                <img alt="image" className="w-260 h-380 object-cover rounded-md shadow-lg" src={bar} /></div>
 
+
+              <div></div>
               <div className="flex flex-col p-8 bg-white rounded shadow-lg">
                 <b className="b flex flex-row text-gray-500">TOP SELLING ITEMS</b>
-                <canvas className='p-5' id='chartRadar'></canvas>
+
+                <img alt="image" className="w-260 h-380 object-cover rounded-md shadow-lg" src={chart} />
               </div>
+
 
             </div>
             {/*grid end here*/}
@@ -94,9 +99,9 @@ const Dashboard = () => {
 
 
         </div>
-      </div>
+      </div >
 
-    </div>
+    </div >
 
   )
 }
--- src/pages/tailor/Fabrics.jsx
+++ src/pages/tailor/Fabrics.jsx
@@ -1,9 +1,8 @@
+
 import button1 from "../../assets/images/fabrics/pattern/pattern- navy blue with red strips.jpg"
 import button2 from "../../assets/images/fabrics/pattern/pattern-ark aegean suit.jpg"
 import button3 from "../../assets/images/fabrics/pattern/pattern-black small square textured.jpg"
 import button4 from "../../assets/images/fabrics/pattern/pattern-blue and navy check.jpg"
-
-import Costume5 from '../../assets/images/costume5.jpeg'
 
 const ViewButtons = () => {
 
@@ -25,17 +24,17 @@ const ViewButtons = () => {
                                     <label className="sr-only">Search</label>
                                     <div className="relative">
                                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                            <svg className="w-4 h-4  dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                            <svg aria-hidden="true" className="w-4 h-4  dark:text-gray-700" fill="none" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
                                             </svg>
                                         </div>
-                                        <input type="text" id="table-search" className="block p-2 pl-10 text-sm text-black border border-gray-300 rounded-3xl w-60
-                                         focus:border-gray-400 " placeholder="Search" />
+                                        <input className="block p-2 pl-10 text-sm text-black border border-gray-300 rounded-3xl w-60
+                                         focus:border-gray-400 " id="table-search" placeholder="Search" type="text" />
                                     </div>
                                 </div>
                                 <div className=" ml-9 flex">
                                     {/* <label for="Sort" className="text-sm font-medium text-gray-900 ">Small select</label> */}
-                                    <select id="Sort" className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                                    <select className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" id="Sort">
                                         <option selected>Sort By</option>
                                         <option value="US">All</option>
                                         <option value="CA">ASC</option>
@@ -50,24 +49,24 @@ const ViewButtons = () => {
                                     <table className="w-full text-sm text-left text-gray-400 rounded-3xl ">
                                         <thead className="text-xs text-gray-500 uppercase bg-gray-100 rounded-3xl">
                                             <tr>
-                                                <th scope="col" className="px-6 py-3">
+                                                <th className="px-6 py-3" scope="col">
                                                     Button Pic
                                                 </th>
-                                                <th scope="col" className="px-6 py-3">
+                                                <th className="px-6 py-3" scope="col">
                                                     Button Id
 
                                                 </th>
-                                                <th scope="col" className="px-6 py-3">
+                                                <th className="px-6 py-3" scope="col">
                                                     Quantity
                                                 </th>
-                                                <th scope="col" className="px-6 py-3">
+                                                <th className="px-6 py-3" scope="col">
                                                     Last updated
                                                 </th>
-                                                
-                                                <th scope="col" className="px-6 py-3">
+
+                                                <th className="px-6 py-3" scope="col">
                                                     Price
                                                 </th>
-                                                <th scope="col" className="px-6 py-3">
+                                                <th className="px-6 py-3" scope="col">
                                                     Option
                                                 </th>
 
@@ -79,7 +78,7 @@ const ViewButtons = () => {
                                         <tbody>
                                             <tr className="bg-white border rounded-3xl hover:bg-gray-300 text-black whitespace-nowrap font-medium">
                                                 <td className="px-4 py-4">
-                                                    <img className="w-16 h-18 object-cover rounded-md shadow-lg" src={button1} alt="image" />
+                                                    <img alt="image" className="w-16 h-18 object-cover rounded-md shadow-lg" src={button1} />
                                                 </td>
                                                 <td className="px-6 py-4 text-center">
                                                     3
@@ -90,43 +89,19 @@ const ViewButtons = () => {
                                                 <td className="px-6 py-4">
                                                     Floyd Miles
                                                 </td>
-                                                
+
                                                 <td className="px-6 py-4">
                                                     Collected
                                                 </td>
                                                 <td className="px-6 py-4 text-right">
-                                                    <a href="#" className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
-                                                     hover:bg-blue-600 hover:underline hover:text-blue-200">Edit</a>
-                                                </td>
-                                            </tr>
- 
-                                            <tr className="bg-white border rounded-3xl hover:bg-gray-300 text-black whitespace-nowrap font-medium">
-                                                <td className="px-4 py-4">
-                                                    <img className="w-16 h-18 object-cover rounded-md shadow-lg" src={button2} alt="image" />
-                                                </td>
-                                                <td className="px-6 py-4 text-center">
-                                                    3
-                                                </td>
-                                                <td className="px-6 py-4">
-                                                    +91 9876543210
-                                                </td>
-                                                <td className="px-6 py-4">
-                                                    Floyd Miles
-                                                </td>
-                                                
-                                                <td className="px-6 py-4">
-                                                    Collected
-                                                </td>
-                                                <td className="px-6 py-4 text-right">
-                                                    <a href="#" className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
-                                                     hover:bg-blue-600 hover:underline hover:text-blue-200">Edit</a>
+                                                    <a className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
+                                                     hover:bg-blue-600 hover:underline hover:text-blue-200" href="#">Edit</a>
                                                 </td>
                                             </tr>
 
                                             <tr className="bg-white border rounded-3xl hover:bg-gray-300 text-black whitespace-nowrap font-medium">
                                                 <td className="px-4 py-4">
-                                                <img className="w-16 h-18 object-cover rounded-md shadow-lg" src={button4} alt="image" />
-                                                 
+                                                    <img alt="image" className="w-16 h-18 object-cover rounded-md shadow-lg" src={button2} />
                                                 </td>
                                                 <td className="px-6 py-4 text-center">
                                                     3
@@ -137,20 +112,20 @@ const ViewButtons = () => {
                                                 <td className="px-6 py-4">
                                                     Floyd Miles
                                                 </td>
-                                              
+
                                                 <td className="px-6 py-4">
                                                     Collected
                                                 </td>
                                                 <td className="px-6 py-4 text-right">
-                                                    <a href="#" className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
-                                                     hover:bg-blue-600 hover:underline hover:text-blue-200">Edit</a>
+                                                    <a className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
+                                                     hover:bg-blue-600 hover:underline hover:text-blue-200" href="#">Edit</a>
                                                 </td>
                                             </tr>
-                                       
 
                                             <tr className="bg-white border rounded-3xl hover:bg-gray-300 text-black whitespace-nowrap font-medium">
                                                 <td className="px-4 py-4">
-                                                    <img className="w-16 h-18 object-cover rounded-md shadow-lg" src={button3} alt="image" />
+                                                    <img alt="image" className="w-16 h-18 object-cover rounded-md shadow-lg" src={button4} />
+
                                                 </td>
                                                 <td className="px-6 py-4 text-center">
                                                     3
@@ -161,17 +136,41 @@ const ViewButtons = () => {
                                                 <td className="px-6 py-4">
                                                     Floyd Miles
                                                 </td>
-                                                
+
                                                 <td className="px-6 py-4">
                                                     Collected
                                                 </td>
                                                 <td className="px-6 py-4 text-right">
-                                                    <a href="#" className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
-                                                     hover:bg-blue-600 hover:underline hover:text-blue-200">Edit</a>
+                                                    <a className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
+                                                     hover:bg-blue-600 hover:underline hover:text-blue-200" href="#">Edit</a>
                                                 </td>
                                             </tr>
-                                         
-                                       
+
+
+                                            <tr className="bg-white border rounded-3xl hover:bg-gray-300 text-black whitespace-nowrap font-medium">
+                                                <td className="px-4 py-4">
+                                                    <img alt="image" className="w-16 h-18 object-cover rounded-md shadow-lg" src={button3} />
+                                                </td>
+                                                <td className="px-6 py-4 text-center">
+                                                    3
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    +91 9876543210
+                                                </td>
+                                                <td className="px-6 py-4">
+                                                    Floyd Miles
+                                                </td>
+
+                                                <td className="px-6 py-4">
+                                                    Collected
+                                                </td>
+                                                <td className="px-6 py-4 text-right">
+                                                    <a className="px-4 py-2 text-blue-100 no-underline bg-blue-500 rounded
+                                                     hover:bg-blue-600 hover:underline hover:text-blue-200" href="#">Edit</a>
+                                                </td>
+                                            </tr>
+
+
                                         </tbody>
                                     </table>
                                 </div>
