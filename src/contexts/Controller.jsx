3775409bcf0dc5d58844b324332920a57aca5339
Binula Rasanjith <binula28@gmail.com>
2023-08-17 07:28:30 +0530

    prop validated

--- src/contexts/Controller.jsx
+++ src/contexts/Controller.jsx
@@ -1,46 +1,43 @@
+import PropTypes from "prop-types";
 import { createContext, useContext, useState } from "react";
 
 const CustomizationContext = createContext({});
 
+export const CustomizationProvider = ({ children }) => {
+	const [material, setMaterial] = useState("");
+	const [PocketFlaps, setPocketFlaps] = useState("false");
+	const [button, setButton] = useState(false);
+	const [three, setThree] = useState(false);
+	const [four, setFour] = useState(false);
+	const [two, setTwo] = useState(false);
 
-export const CustomizationProvider = (props) => {
-
-    const [material, setMaterial] = useState("");
-    const [PocketFlaps, setPocketFlaps] = useState("false");
-    const [button, setButton] = useState(false);
-    const [three, setThree] = useState(false);
-    const [four, setFour] = useState(false);
-    const [two, setTwo] = useState(false);
-
-    return (
-        <CustomizationContext.Provider
-            value={{
-                material,
-                setMaterial,
-                PocketFlaps,
-                setPocketFlaps,
-                button,
-                setButton,
-                three,
-                setThree,
-                four,
-                setFour,
-                two,
-                setTwo
-
-
-
-            }}
-        >
-            {props.children}
-        </CustomizationContext.Provider>
-    );
+	return (
+		<CustomizationContext.Provider
+			value={{
+				material,
+				setMaterial,
+				PocketFlaps,
+				setPocketFlaps,
+				button,
+				setButton,
+				three,
+				setThree,
+				four,
+				setFour,
+				two,
+				setTwo,
+			}}
+		>
+			{children}
+		</CustomizationContext.Provider>
+	);
 };
 
-
-
+CustomizationProvider.propTypes = {
+	children: PropTypes.node.isRequired,
+};
 
 export const useCustomization = () => {
-    const context = useContext(CustomizationContext);
-    return context;
+	const context = useContext(CustomizationContext);
+	return context;
 };
