e2e8747be8bac8816f2e1f7d61d33a092ce99f81
Binula Rasanjith <binula28@gmail.com>
2023-08-17 07:26:04 +0530

    supplier apis added to frontend

--- src/api/supplierAPI.js
+++ src/api/supplierAPI.js
@@ -1,5 +1,29 @@
 import api from "./api";
 
+/* 
+router.post("/create", createSupplier);
+router.get("/get", getSuppliers);
+router.get("/get/:id", getSupplier);
+router.patch("/update", updateSupplier);
+router.delete("/delete/:id", deleteSupplier);
+*/
+
 export const addSupplier = (data) => {
     return api.post('/user/add-supplier', data);
+}
+
+export const getSuppliers = () => {
+    return api.get('/user/get-suppliers');
+}
+
+export const getSupplier = (id) => {
+    return api.get(`/user/get-supplier/${id}`);
+}
+
+export const updateSupplier = (data) => {
+    return api.patch('/user/update-supplier', data);
+}
+
+export const deleteSupplier = (id) => {
+    return api.delete(`/user/delete-supplier/${id}`);
 }
