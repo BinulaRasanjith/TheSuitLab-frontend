4ac8585da40ca76f2492362c2bb21a1aeddc7778
Shamalka_M <2020cs111@stu.ucsc.cmb.ac.lk>
2023-09-12 19:46:50 +0530

    payment UI

--- src/components/Customer/Cart.jsx
+++ src/components/Customer/Cart.jsx
@@ -1,5 +1,9 @@
 import { Button } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
+import SuitImage1 from '../../assets/images/rentsuits/caremal suit/1.webp';
+import SuitImage7 from '../../assets/images/rentsuits/black paisley suit/1.webp';
+import SuitImage13 from '../../assets/images/rentsuits/burgandy strech/1.webp';
+import { useNavigate } from 'react-router-dom';
 
 import { getCart } from "../../api/customerAPI";
 
@@ -11,6 +15,14 @@ const Cart = () => {
 			const cartItemsFromServer = await getCart();
 			setCartItems(cartItemsFromServer);
 		};
+    const navigate = useNavigate();
+
+    const handleCheckout = () => {
+        navigate('/customer/payment');
+    };
+
+    // Sample data for cart items (you can replace this with your data)
+    //const [cartItemsArray, setCartItems] = useState([]);
 
 		getCartItems();
 		console.log(cartItems);
@@ -85,27 +97,69 @@ const Cart = () => {
 							))}
 					</div>
 
-					<div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
-						<div className="mb-2 flex justify-between">
-							<p className="text-gray-700">Subtotal</p>
-							<p className="text-gray-700">Rs 134980</p>
-						</div>
-						<hr className="my-4" />
-						<div className="flex justify-between">
-							<p className="text-lg font-bold">Total</p>
-							<div className="">
-								<p className="mb-1 text-lg font-bold">Rs 134980</p>
-								<p className="text-sm text-gray-700">including VAT</p>
-							</div>
-						</div>
-						<button className="mt-6 w-full rounded-md bg-black py-1.5 font-medium text-blue-50 hover:bg-blue-600">
-							Check out
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                                    {/* }}
+                                    onClick={() => removeItemFromCart(item.id)}
+                                >
+                                    X
+                                </Button>
+                                <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                                    <img src={item.image} alt="product-image" className="w-full rounded-lg sm:w-40" />
+                                    <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                                        <div className="mt-5 sm:mt-0">
+                                            <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
+                                            <p className="mt-1 text-xs text-gray-700">{item.size}</p>
+                                        </div>
+                                        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                            <div className="flex items-center border-gray-100">
+                                                <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50"> - </span>
+                                                <input className="h-8 w-8 border bg-white text-center text-xs outline-none" type="number" value={item.quantity} min="1" />
+                                                <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"> + </span>
+                                            </div>
+                                            <div className="flex items-center space-x-4">
+                                                <p className="text-sm">{item.price}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div> */}
+
+                    <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                        <div className="mb-2 flex justify-between">
+                            <p className="text-gray-700">Subtotal</p>
+                            <p className="text-gray-700">Rs 134980</p>
+                        </div>
+                        <hr className="my-4" />
+                        <div className="flex justify-between">
+                            <p className="text-lg font-bold">Total</p>
+                            <div className="">
+                                <p className="mb-1 text-lg font-bold">Rs 134980</p>
+                                <p className="text-sm text-gray-700">including VAT</p>
+                            </div>
+                        </div>
+                       
+                        <Button
+                         className="block w-full p-5 mt-4"
+                         rounded={"md"}
+                         color={"white"}
+                         bgColor={"black"}
+                         size="sm"
+                         _hover={{
+                             bg: "blue",
+                             color: "blue-50",
+
+                         }}
+                         onClick={handleCheckout()}
+                         //onClick={() => navigate("/customer/payment")}
+                        >
+                            Check out
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
