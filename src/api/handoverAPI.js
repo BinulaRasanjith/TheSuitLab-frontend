40c81e5e3b264407ed98e927bfda075086370831
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-10-18 23:21:24 +0530

    Update handoverAPI.js with recommended RESTful API practices

--- src/api/handoverAPI.js
+++ src/api/handoverAPI.js
@@ -1,17 +1,24 @@
 import api from "./api";
 
+// ADD NEW HIRE
 export const newHire = (data) => {
-    return api.post('/hiring/new-hire', data);
+    return api.post('/hiring/', data);
 }
 
+// VIEW ALL HIRES
 export const getHires = () => {
-    return api.get('/hiring/get-hires');
+    return api.get('/hiring/');
+    // return api.get('/hiring/get-hires');
 }
 
+// UPDATE REQUESTED HIRE
 export const updateHire = (new_data) => {
-    return api.post('/hiring/update-hires', new_data);
+    return api.put('/hiring/', new_data);
+    // return api.post('/hiring/update-hire', new_data);
 }
 
+// CANCEL REQUESTED HIRE
 export const cancelHire = (id) => {
-    return api.get(`/hiring/remove-hire/${id}`);
+    return api.delete(`/hiring/${id}`);
+    // return api.get(`/hiring/remove-hire/${id}`);
 }
