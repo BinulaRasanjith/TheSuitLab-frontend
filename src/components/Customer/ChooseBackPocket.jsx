0a3fca633eaaef10aafee72675ff71554eb13055
Shamalka_M <2020cs111@stu.ucsc.cmb.ac.lk>
2023-11-01 01:32:39 +0530

    pocket select state

--- src/components/Customer/ChooseBackPocket.jsx
+++ src/components/Customer/ChooseBackPocket.jsx
@@ -1,16 +1,18 @@
 // import Curved from '../../assets/images/back_pocket_styles/Curved (1).png'
 // import Square from '../../assets/images/back_pocket_styles/Square.png'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Single from '../../assets/images/back_pocket_styles/Single.png'
 import Double from '../../assets/images/back_pocket_styles/withbtn.png'
 // import Modern from '../../assets/images/back_pocket_styles/withbtn.png'
-import { setJacket } from '../../store/slices/jacketCustomizationSlice'
+import { setJacket, selectJacket } from '../../store/slices/jacketCustomizationSlice'
 import { setRotation } from "../../store/slices/rotationHandleSlice"
+import { MdDoneOutline } from "react-icons/md";
 
 
 const ChooseBackPocket = () => {
     const dispatch = useDispatch()
+    const backPocketSelected = useSelector(selectJacket).backPocket;
 
     return (
 
@@ -22,14 +24,24 @@ const ChooseBackPocket = () => {
                 <div onClick={() => {
                     dispatch(setJacket({ backPocket: "no-btn" }))
                     dispatch(setRotation(Math.PI));
-                }} className='flex flex-col items-center p-3 justify-center cursor-pointer rounded-lg border border-black-2  shadow'>
+                }} className='flex relative flex-col items-center p-3 justify-center cursor-pointer rounded-lg border border-black-2  shadow'>
+                    {backPocketSelected === 'no-btn' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="white" />
+                        </div>
+                    )}
                     <img alt="Single Opening" className='pb-5' src={Single} />
                     <label className='text-sm'>POCKET</label>
                 </div>
                 <div onClick={() => {
                     dispatch(setJacket({ backPocket: "with-btn" }))
                     dispatch(setRotation(Math.PI));
-                }} className='flex flex-col items-center p-3 justify-center cursor-pointer rounded-lg border border-black-2  shadow'>
+                }} className='flex relative flex-col items-center p-3 justify-center cursor-pointer rounded-lg border border-black-2  shadow'>
+                    {backPocketSelected === 'with-btn' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="white" />
+                        </div>
+                    )}
                     <img alt="Double Opening" className='' src={Double} />
                     <label className='text-sm'>POCKET WITH BUTTON</label>
                 </div>
--- src/components/Customer/ChooseButtons.jsx
+++ src/components/Customer/ChooseButtons.jsx
@@ -1,14 +1,16 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 import oneButton from "../../assets/images/button_styles/1Button.png"
 import twoButton from "../../assets/images/button_styles/2Button.png"
 import fourDTwo from "../../assets/images/button_styles/4D2.png"
 import sixDThree from "../../assets/images/button_styles/6D3.png"
-import { setJacket } from "../../store/slices/jacketCustomizationSlice"
+import { setJacket, selectJacket } from "../../store/slices/jacketCustomizationSlice"
 import { setRotation } from "../../store/slices/rotationHandleSlice"
+import { MdDoneOutline } from "react-icons/md";
 
 const ChooseButtons = () => {
     const dispatch = useDispatch()
+    const buttonSelected = useSelector(selectJacket).button;
 
     return (
         <div className="flex my-5 flex-col px-3">
@@ -17,14 +19,24 @@ const ChooseButtons = () => {
                 <div onClick={() => {
                     dispatch(setJacket({ button: '1S' }))
                     dispatch(setRotation(2 * Math.PI));
-                }} className="flex flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                }} className="flex relative flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                    {buttonSelected === '1S' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="black" />
+                        </div>
+                    )}
                     <img alt="" className="pb-3" src={oneButton} />
                     <label className="text-sm">1 BUTTON,SINGLE <br />BREASTED</label>
                 </div>
                 <div onClick={() => {
                     dispatch(setJacket({ button: '2S' }))
                     dispatch(setRotation(2 * Math.PI));
-                }} className="flex flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                }} className="flex relative flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                    {buttonSelected === '2S' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="black" />
+                        </div>
+                    )}
                     <img alt="" className="pb-3" src={twoButton} />
                     <label className="text-sm">2 BUTTONS,SINGLE <br />BREASTED</label>
                 </div>
@@ -33,7 +45,12 @@ const ChooseButtons = () => {
                     dispatch(setJacket({ button: '4D2' }))
                     dispatch(setRotation(2 * Math.PI));
                 }
-                } className="flex flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                } className="flex relative flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2 shadow ">
+                    {buttonSelected === '4D2' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="black" />
+                        </div>
+                    )}
                     <img alt="" className="pb-3" src={fourDTwo} />
                     <label className="text-sm">4 BUTTONs,DOUBLE <br />BREASTED (2 TO)</label>
                 </div>
@@ -42,7 +59,12 @@ const ChooseButtons = () => {
                     dispatch(setJacket({ button: '6D3' }))
                     dispatch(setRotation(2 * Math.PI));
                 }
-                } className="flex flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2  shadow ">
+                } className="flex relative flex-col p-3 items-center cursor-pointer rounded-lg border border-black-2  shadow ">
+                    {buttonSelected === '6D3' && (
+                        <div className="absolute z-5 top-2 right-2">
+                            <MdDoneOutline size={24} color="black" />
+                        </div>
+                    )}
                     <img alt="" className="pb-3" src={sixDThree} />
                     <label className="text-sm">6 BUTTONs,DOUBLE <br />BREASTED (3 TO)</label>
                 </div>
