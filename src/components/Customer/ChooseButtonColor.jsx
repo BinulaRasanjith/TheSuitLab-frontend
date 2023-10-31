37f0b53934ec0222b93ec1891811817f4a5fb724
Shamalka_M <2020cs111@stu.ucsc.cmb.ac.lk>
2023-11-01 01:17:43 +0530

    Update ChooseButtonColor.jsx

--- src/components/Customer/ChooseButtonColor.jsx
+++ src/components/Customer/ChooseButtonColor.jsx
@@ -14,18 +14,22 @@ import purple_button from '../../assets/images/button_colors/purple.png'
 import silver_button from '../../assets/images/button_colors/silver.png'
 import sky_blue_button from '../../assets/images/button_colors/sky blue.png'
 import white_button from '../../assets/images/button_colors/white.png'
-import { setJacket } from "../../store/slices/jacketCustomizationSlice"
+import { setJacket, selectJacket } from "../../store/slices/jacketCustomizationSlice"
 import { setRotation } from "../../store/slices/rotationHandleSlice"
-
+import { MdDoneOutline } from "react-icons/md";
+import { useSelector } from 'react-redux';
 
 const ChooseButtonColor = () => {
     const dispatch = useDispatch()
     const location = useLocation();
+    const buttonColorSelected = useSelector(selectJacket).buttonColor;
+
     return (
         <div className='flex flex-col'>
             <span className="p-5 text-2xl font-bold">Choose Button Colors</span>
             <div className='p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-5'>
-                <button onClick={() => {
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'none' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
@@ -33,96 +37,189 @@ const ChooseButtonColor = () => {
                         dispatch(setRotation(2 * Math.PI));
                     }
                 }}>
+                    {buttonColorSelected === 'none' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
                     <img alt="" className="pb-3 h-24" src={None} />
                 </button>
-                <button onClick={() => {
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'black' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={black_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'black' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={black_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'brown' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={brown_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'brown' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={brown_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'deepBrown' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={deep_brown_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'deepBrown' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={deep_brown_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'lightKhaki' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={light_khaki_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'lightKhaki' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={light_khaki_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'maroon' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={maroon_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'maroon' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={maroon_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'navy' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={navy_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'navy' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={navy_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'pearlWhite' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={pearl_white_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'pearlWhite' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={pearl_white_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'purple' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={purple_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'purple' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={purple_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'silver' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={silver_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'silver' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={silver_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'skyBlue' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={sky_blue_button} /></button>
-                <button onClick={() => {
+                }}>
+                    {buttonColorSelected === 'skyBlue' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={sky_blue_button} />
+                </button>
+
+                <button className="relative" onClick={() => {
                     dispatch(setJacket({ buttonColor: 'white' }))
                     if (location.pathname.includes('pant')) {
                         dispatch(setRotation(Math.PI));
                     } else {
                         dispatch(setRotation(2 * Math.PI));
                     }
-                }}><img alt="" className='h-20 w-24' src={white_button} /></button>
+                }}>
+                    {buttonColorSelected === 'white' && (
+                        <div className="absolute z-5 top-0 right-2">
+                            <MdDoneOutline size={24} color="green" />
+                        </div>
+                    )}
+                    <img alt="" className='h-20 w-24' src={white_button} />
+                </button>
 
             </div>
         </div>
