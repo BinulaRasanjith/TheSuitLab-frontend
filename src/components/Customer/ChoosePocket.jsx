b25b59115866e80581593d3aa8f0b03a44a6a169
Shamalka_M <2020cs111@stu.ucsc.cmb.ac.lk>
2023-11-01 01:44:20 +0530

    Update ChoosePocket.jsx

--- src/components/Customer/ChoosePocket.jsx
+++ src/components/Customer/ChoosePocket.jsx
@@ -6,13 +6,16 @@ import PKFive from "../../assets/images/pocket_styles/PK-5 1.png";
 import PKSix from "../../assets/images/pocket_styles/PK-6 1.png";
 // import PKSeven from "../../assets/images/pocket_styles/PK-7 1.png";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { setJacket } from "../../store/slices/jacketCustomizationSlice";
+import { setJacket, selectJacket } from "../../store/slices/jacketCustomizationSlice";
 import { setRotation } from "../../store/slices/rotationHandleSlice"
+import { MdDoneOutline } from "react-icons/md";
 
 const ChoosePocket = () => {
 	const dispatch = useDispatch();
+	const pocketSelected = useSelector(selectJacket).pocket;
+
 	return (
 		<div className="flex flex-col gap-10 my-5 px-3">
 			<div>
@@ -24,8 +27,13 @@ const ChoosePocket = () => {
 						dispatch(setJacket({ pocket: "2-straight-flaps" }))
 						dispatch(setRotation(2 * Math.PI));
 					}}
-						className="flex p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow"
+						className="flex relative p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow"
 					>
+						{pocketSelected === '2-straight-flaps' && (
+							<div className="absolute z-5 top-2 right-2">
+								<MdDoneOutline size={24} color="black" />
+							</div>
+						)}
 						<img alt="" className="pb-2" src={PKOne} />
 						<label className="text-sm">
 							2 STRAIGHT <br /> POCKETS
@@ -34,7 +42,12 @@ const ChoosePocket = () => {
 					<div onClick={() => {
 						dispatch(setJacket({ pocket: "2-straight-flaps-1-ticket" }))
 						dispatch(setRotation(2 * Math.PI));
-					}} className="flex p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow">
+					}} className="flex relative p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow">
+						{pocketSelected === '2-straight-flaps-1-ticket' && (
+							<div className="absolute z-5 top-2 right-2">
+								<MdDoneOutline size={24} color="black" />
+							</div>
+						)}
 						<img alt="" className="pb-2" src={PKTwo} />
 						<label className="text-sm">
 							2 STRAIGHT POCKETS & <br /> 1 TICKET POCKET
@@ -45,8 +58,13 @@ const ChoosePocket = () => {
 						dispatch(setJacket({ pocket: "2-straight" }))
 						dispatch(setRotation(2 * Math.PI))
 					}}
-						className="flex p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow"
+						className="flex relative p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow"
 					>
+						{pocketSelected === '2-straight' && (
+							<div className="absolute z-5 top-2 right-2">
+								<MdDoneOutline size={24} color="black" />
+							</div>
+						)}
 						<img alt="" className="pb-2" src={PKFive} />
 						<label className="text-sm">
 							2 STRAIGHT POCKETS
@@ -57,7 +75,12 @@ const ChoosePocket = () => {
 					<div onClick={() => {
 						dispatch(setJacket({ pocket: "2-straight-1-ticket" }))
 						dispatch(setRotation(2 * Math.PI));
-					}} className="flex p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow">
+					}} className="flex relative p-2 flex-col justify-center items-center cursor-pointer rounded-lg border border-black-2  shadow">
+						{pocketSelected === '2-straight-1-ticket' && (
+							<div className="absolute z-5 top-2 right-2">
+								<MdDoneOutline size={24} color="black" />
+							</div>
+						)}
 						<img alt="" className="pb-2" src={PKSix} />
 						<label className="text-sm">
 							2 STRAIGHT POCKETS
