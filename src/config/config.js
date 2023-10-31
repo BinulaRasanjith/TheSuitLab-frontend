0fdc0ec000b74ef9f614726eee71ac26c6c7e448
Thimet <92925044+Thimet@users.noreply.github.com>
2023-10-31 17:36:20 +0530

    created accessory controller to add to cart

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://localhost:3333'; // TODO: get this from .env
+export const SERVER_URL = "http://localhost:3333"; // TODO: get this from .env
 
 export const API_URL = `${SERVER_URL}/api`; // TODO: use this
 
@@ -7,3 +7,5 @@ export const PROFILE_PICTURE_URL = `${SERVER_URL}/uploads/profile-pics`;
 export const MATERIAL_IMAGES_URL = `${SERVER_URL}/uploads/material-images`;
 
 export const HIRE_COSTUME_IMAGES_URL = `${SERVER_URL}/uploads/hire-costumes-images`;
+
+export const ACCESSORY_IMAGES_URL = `${SERVER_URL}/uploads/accessories-images`;
