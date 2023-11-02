7a26f57ce87f3194267ca7511acd15443344324c
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-11-02 11:43:39 +0530

    page updated

--- src/pages/Assistant/Materials.jsx
+++ src/pages/Assistant/Materials.jsx
@@ -7,9 +7,10 @@ import { getMaterials } from '../../api/materialAPI'
 import SearchBox from '../../components/Assistant/Controls/HeaderSearchBox'
 import Pagination from '../../components/Assistant/Controls/Pagination'
 import MaterialStockUpdateForm from "../../components/Assistant/Forms/MaterialUpdateForm"
-
+import { MATERIAL_IMAGES_URL } from '../../config/config'
 
 const Materials = () => {
+
     const [materials, setMaterials] = useState([]);
     const [filteredMaterials, setFilteredMaterials] = useState([]);
 
@@ -20,7 +21,7 @@ const Materials = () => {
     });
 
     const [currentPage, setCurrentPage] = useState(1);
-    const recordsPerPage = 5;
+    const recordsPerPage = 4;
 
     const startIndex = (currentPage - 1) * recordsPerPage;
     const endIndex = startIndex + recordsPerPage;
@@ -102,46 +103,46 @@ const Materials = () => {
                         </thead>
                         <tbody className=" text-left text-md font-medium text-gray-400 w-full">
                             {filteredMaterials.length === 0 ? <tr> No data to display </tr> :
-                            filteredMaterials.slice(startIndex, endIndex).map((item, index) => (
-                                <tr key={index} className="items-center text-centers border-b-2 hover:bg-gray-300 text-black whitespace-nowrap font-medium w-full">
-                                    <td className="py-4 w-72 hidden"> <Link to={`${item.materialCode}`}>{item.materialCode}</Link></td>
-                                    <td className="py-4 w-72"><img src={item.image} alt="Material Image" /></td>
-                                    <td className="py-4 w-72">{item.materialName}</td>
-                                    <td className="py-4 w-60">{item.color}</td>
-                                    <td className="py-4 w-60">{item.unitPrice}</td>
-                                    <td className="py-4 w-60">{item.quantity}</td>
-                                    <td className="py-4 w-60">
-                                        <Button
-                                            _hover={{
-                                                bg: '#6B9DCA',
-                                                textColor: 'white'
-                                            }}
-                                            bg={'#BEE7FF80'}
-                                            border={'1px'}
-                                            borderColor={'#6B9DCA'}
-                                            height={'2rem'}
-                                            onClick={handleFormOpen}
-                                            textColor={'#6B9DCA'}
-                                            width={'5.5rem'}>
-                                            Update
-                                        </Button>
-                                    </td>
-                                </tr>
-                            ))}
+                                filteredMaterials.slice(startIndex, endIndex).map((item, index) => (
+                                    <tr key={index} className="items-center text-centers border-b-2 text-black whitespace-nowrap font-medium w-full">
+                                        <td className="py-4 w-72 hidden"> <Link to={`${item.materialCode}`}>{item.materialCode}</Link></td>
+                                        <td className="py-4 w-72"><img className="rounded" width={64} src={`${MATERIAL_IMAGES_URL}/${item.image}`} alt="Material Image" /></td>
+                                        <td className="py-4 w-72">{item.materialName}</td>
+                                        <td className="py-4 w-60">{item.color}</td>
+                                        <td className="py-4 w-60">Rs. {item.unitPrice.toFixed(2)}</td>
+                                        <td className="py-4 w-60">{item.quantity}</td>
+                                        <td className="py-4 w-60">
+                                            <Button
+                                                _hover={{
+                                                    bg: '#6B9DCA',
+                                                    textColor: 'white'
+                                                }}
+                                                bg={'#BEE7FF80'}
+                                                border={'1px'}
+                                                borderColor={'#6B9DCA'}
+                                                height={'2rem'}
+                                                onClick={handleFormOpen}
+                                                textColor={'#6B9DCA'}
+                                                width={'5.5rem'}>
+                                                Update
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
                         </tbody>
                     </table>
                 </div>
                 <div className=" flex justify-between">
-                        <div className=" py-3 text-sm font-medium text-neutral-400">
-                            Showing data {startIndex + 1} to {endIndex} of {filteredMaterials.length} entries
-                        </div>
-                        <div className=" py-3">
-                            <Pagination
-                                currentPage={currentPage}
-                                totalPages={Math.ceil(materials.length / recordsPerPage)}
-                                onPageChange={(newPage) => setCurrentPage(newPage)}
-                            />
-                        </div>
+                    <div className=" py-3 text-sm font-medium text-neutral-400">
+                        Showing data {startIndex + 1} to {endIndex} of {filteredMaterials.length} entries
+                    </div>
+                    <div className=" py-3">
+                        <Pagination
+                            currentPage={currentPage}
+                            totalPages={Math.ceil(materials.length / recordsPerPage)}
+                            onPageChange={(newPage) => setCurrentPage(newPage)}
+                        />
+                    </div>
                 </div>
             </div>
 
